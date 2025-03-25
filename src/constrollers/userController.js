c613edf10d0db7beac32195f9eefba9683ca9658
BrainWosher <svprokopenko92@gmail.com>
2025-03-25 15:38:20 +0700

    добавил слой сервисов, переиспользовал их в контроллерах

--- src/constrollers/userController.js
+++ src/constrollers/userController.js
@@ -1,19 +1,26 @@
+const userService = require('../services/userService');
+
 const getAllUsers = (req, res) => {
+  const allUsers = userService.getAllUsers();
   res.send('Get all users');
 };
 const getOneUser = (req, res) => {
+  const user = userService.getOneUser();
   res.send('Get an existing User');
 };
 
 const createNewUser = (req, res) => {
+  const createdUser = userService.createNewUser();
   res.send('Create a new User');
 };
 
 const updateOneUser = (req, res) => {
+  const updateUser = userService.updateOneUser();
   res.send('Update an existing User');
 };
 
 const deleteOneUser = (req, res) => {
+  userService.deleteOneUser();
   res.send('Delete an existing User');
 };
 
