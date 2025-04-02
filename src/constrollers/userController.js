7ed929ee67a8d7dc0513734908f9ce5c791465fe
BrainWosher <svprokopenko92@gmail.com>
2025-04-03 00:26:15 +0700

    контроллер получения всех юзеров и создание нового

--- src/constrollers/userController.js
+++ src/constrollers/userController.js
@@ -1,17 +1,33 @@
 const userService = require('../services/userService');
 
-const getAllUsers = (req, res) => {
-  const allUsers = userService.getAllUsers();
-  res.send('Get all users');
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await userService.getAllUsers();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
 };
 const getOneUser = (req, res) => {
   const user = userService.getOneUser();
   res.send('Get an existing User');
 };
 
-const createNewUser = (req, res) => {
-  const createdUser = userService.createNewUser();
-  res.send('Create a new User');
+const createNewUser = async (req, res) => {
+  try {
+    const user = req.body;
+    const { name, surname, email } = user;
+    if (!name || !surname || !email) {
+      res.status(400).json({ message: 'Все поля обязательны' });
+    }
+    const newUser = await userService.createNewUser(newUser);
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
 };
 
 const updateOneUser = (req, res) => {
